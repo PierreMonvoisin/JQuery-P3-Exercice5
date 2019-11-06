216c0b291f3ab432a0f7a0ae133ed608f6966854
PierreMonvoisin <monvoisin.pierre@gmail.com>
2019-11-06 12:26:51 +0100

    Exercice presque terminé, problème avec le carré quand il touche le bas et la droite )

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,22 +1,31 @@
 $(function() {
-  $('#carre').css({'height': '100px', 'width': '100px','background-color' : 'red'});
-  let i = 10;
+  $('#carre').css({'height': '100px', 'width': '100px','background-color' : 'red','position' : 'absolute'});
+  var windowWidth = $(window).width();
+  var windowHeight = $(window).height();
 
   $('input').keypress(function(e) {
     e.preventDefault();
   });
-  $('input').keyup(function(e){
+  $('input').keydown(function(e){
     let direction = e.keyCode;
+    // controle si le carré touche les bords
+    if ($('#carre').offset().left <= 0){$('#carre').animate({right: '5px'}, 'fast')}
+    if (windowWidth < $('#carre').offset().left){$('#carre').animate({left: '5px'}, 'fast')}
+    if (windowHeight < $('#carre').offset().top){$('#carre').animate({top: '5px'}, 'fast')}
+    if ($('#carre').offset().top <= 0){$('#carre').animate({bottom: '5px'}, 'fast')}
+    // controle de la direction en fonction de la fleche appuyée
     if (direction == 37) {
-      var left = $('#carre').offset().left;
-      $('carre').css('left', left);
-      $('#carre').animate({left: '10px'}, 'fast');
+      // gauche
+      $('#carre').animate({right: '+=50px'});
     } else if (direction == 38) {
-      alert('haut');
+      // haut
+      $('#carre').animate({bottom: '+=50px'});
     } else if (direction == 39) {
-      alert('droite');
+      // droite
+      $('#carre').animate({right: '-=50px'});
     } else if (direction == 40) {
-      alert('bas');
+      // bas
+      $('#carre').animate({bottom: '-=50px'});
     }
   });
 
